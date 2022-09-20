034c9c854f8d7ce4b75a03d51514147549f046ea
Rmoreno3 <reynaldomoreno173@gmail.com>
2022-09-19 22:38:31 -0400

    probando los estilos en nextjs

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Navbar } from 'components/Navbar/Navbar';
 
+
 const Layout = ({ children }) => {
   return (
     <div>
       <Navbar />
       {children}
-      <footer>Este es el Footer</footer>
+      <footer className="container">Este es el Footer</footer>
+      <style jsx>{`
+        .container {
+          background-color: salmon;
+        }
+      `}</style>
     </div>
   )
 }
